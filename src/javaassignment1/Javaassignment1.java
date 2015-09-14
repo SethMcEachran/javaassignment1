b2bdee0d8cd3a7a08f026eb33d11b8a87a4813e5
Seth <c0611751@mylambton.ca>
2015-09-14 16:09:33 -0400

    Initial

---
+++ src/javaassignment1/Javaassignment1.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaassignment1;
+
+/**
+ *
+ * @author c0611751
+ */
+public class Javaassignment1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Hello World"); //commenting part iqnored
+        
+        /*
+        *Assignment 1
+        * Job posting: Java Developer
+        *http://www.indeed.ca/cmp/Tundra-Technical-Solution/jobs/Java-Developer-a72a26996f875cbe?q=java
+        * 
+        * swing-GUI widget toolkit https://en.wikipedia.org/wiki/Swing_(Java)
+        * java remote method invocation object-oriented equivalent of remote procedure calls -https://en.wikipedia.org/wiki/Java_remote_method_invocation
+        * java ee -widely used enterprise computing platform developed under the Java Community Process.-https://en.wikipedia.org/wiki/Java_Platform,_Enterprise_Edition
+        * oracle does sql stuff https://en.wikipedia.org/wiki/Oracle_Database
+        * PostgreSQL- is an object-relational database management system (ORDBMS) with an emphasis on extensibility and on standards-compliance. https://en.wikipedia.org/wiki/PostgreSQL
+        * JSON - is an open standard format that uses human-readable text to transmit data objects consisting of attribute–value pairs-https://en.wikipedia.org/wiki/JSON
+        * web service- A Web service communication between two electronic devices over a network. - https://en.wikipedia.org/wiki/Web_service
+        *XML transformation language- is a programming language designed specifically to transform an input XML document into an output document which satisfies some specific goal.https://en.wikipedia.org/wiki/XML_transformation_language
+        *Microsoft SQL Server-s a relational database management system developed by Microsoft-https://en.wikipedia.org/wiki/Microsoft_SQL_Server
+        *.NET Framework- is a software framework developed by Microsoft that runs primarily on Microsoft Windows.-https://en.wikipedia.org/wiki/.NET_Framework
+        *toronto 87k- http://swz.salary.com/CanadaSalaryWizard/Software-Developer-III-Salary-Details-Toronto-ON.aspx
+        * Waterloo 89k -http://swz.salary.com/CanadaSalaryWizard/Software-Developer-III-Salary-Details-Waterloo-ON.aspx
+        *Vancover 84k - http://swz.salary.com/CanadaSalaryWizard/Software-Developer-III-Salary-Details-Vancover-BC.aspx
+        * 
+        */
+    }
+    
+    
+}
