94fe949a7e882f2e258cac7dc8ddca2f9cc5ecc6
Seth McEachran <c0611751@mylambton.ca>
2015-09-15 16:16:30 -0400

    used own words fix
    :)

--- src/javaassignment1/Javaassignment1.java
+++ src/javaassignment1/Javaassignment1.java
@@ -24,16 +24,16 @@ public class Javaassignment1 {
         * Job posting: Java Developer
         *http://www.indeed.ca/cmp/Tundra-Technical-Solution/jobs/Java-Developer-a72a26996f875cbe?q=java
         * 
-        * swing-GUI widget toolkit https://en.wikipedia.org/wiki/Swing_(Java)
-        * java remote method invocation object-oriented equivalent of remote procedure calls -https://en.wikipedia.org/wiki/Java_remote_method_invocation
-        * java ee -widely used enterprise computing platform developed under the Java Community Process.-https://en.wikipedia.org/wiki/Java_Platform,_Enterprise_Edition
+        * swing-a GUI widget toolkit https://en.wikipedia.org/wiki/Swing_(Java)
+        * java remote method does remote procedure calls -https://en.wikipedia.org/wiki/Java_remote_method_invocation
+        * java ee -enterprise edition.-https://en.wikipedia.org/wiki/Java_Platform,_Enterprise_Edition
         * oracle does sql stuff https://en.wikipedia.org/wiki/Oracle_Database
-        * PostgreSQL- is an object-relational database management system (ORDBMS) with an emphasis on extensibility and on standards-compliance. https://en.wikipedia.org/wiki/PostgreSQL
-        * JSON - is an open standard format that uses human-readable text to transmit data objects consisting of attribute–value pairs-https://en.wikipedia.org/wiki/JSON
-        * web service- A Web service communication between two electronic devices over a network. - https://en.wikipedia.org/wiki/Web_service
-        *XML transformation language- is a programming language designed specifically to transform an input XML document into an output document which satisfies some specific goal.https://en.wikipedia.org/wiki/XML_transformation_language
-        *Microsoft SQL Server-s a relational database management system developed by Microsoft-https://en.wikipedia.org/wiki/Microsoft_SQL_Server
-        *.NET Framework- is a software framework developed by Microsoft that runs primarily on Microsoft Windows.-https://en.wikipedia.org/wiki/.NET_Framework
+        * PostgreSQL- also does sql stuff https://en.wikipedia.org/wiki/PostgreSQL
+        * JSON -easy to read -https://en.wikipedia.org/wiki/JSON
+        * web service- hosts websites - https://en.wikipedia.org/wiki/Web_service
+        *XML transformation language- converts xml to another format -https://en.wikipedia.org/wiki/XML_transformation_language
+        *Microsoft SQL Server-smicrosoft's database system online-https://en.wikipedia.org/wiki/Microsoft_SQL_Server
+        *.NET Framework-a software framework for low resourse computers -https://en.wikipedia.org/wiki/.NET_Framework
         *toronto 87k- http://swz.salary.com/CanadaSalaryWizard/Software-Developer-III-Salary-Details-Toronto-ON.aspx
         * Waterloo 89k -http://swz.salary.com/CanadaSalaryWizard/Software-Developer-III-Salary-Details-Waterloo-ON.aspx
         *Vancover 84k - http://swz.salary.com/CanadaSalaryWizard/Software-Developer-III-Salary-Details-Vancover-BC.aspx
